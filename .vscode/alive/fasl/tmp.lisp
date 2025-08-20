9b3a603b58b87e990c5445489400389bef382710
03 <test2@test2.com>
2025-08-20 21:24:01 +0900

    assert文追加

---
+++ .vscode/alive/fasl/tmp.lisp
@@ -0,0 +1,16 @@
+(defun assert-report (result expected actual form &optional caller)
+  "assert (result:結果(expectedとactualの比較) expected:期待値 actual:実際の値 form:計算式 caller:呼び出し元)
+  resultがnilのとき、失敗のメッセージを出力。"
+  (when (not result)
+        (format t "~a 期待値：~a~\t実績値：~a~\t評価式：~a~%" (if caller (concatenate 'string "【" caller "】") "") expected actual form))
+  result)
+(defmacro my-assert (expected fn)
+  "my-assert (expected:期待値 fn：計算式)
+  expectedとfnの結果が同一でないとき、エラー文面(assert-report)を返却。
+  return: reuslt or nil"
+  (let ((result (gensym)))
+    `(let ((,result ,fn))
+       (assert-report (= ,expected (,@result))
+                      ,expected
+                      ,result
+                      ',fn))))
